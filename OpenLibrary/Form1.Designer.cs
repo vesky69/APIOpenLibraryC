28ffda1478f29069d8a4a6a980014c8e17e38af0
Júlia :) <148802056+vesky69@users.noreply.github.com>
2026-06-03 10:11:11 -0300

    Refactor Form1.Designer.cs for clarity and structure

--- OpenLibrary/Form1.Designer.cs
+++ OpenLibrary/Form1.Designer.cs
@@ -10,11 +10,6 @@
         private System.Windows.Forms.DataGridView dgvLivros;
         private System.Windows.Forms.PictureBox picCapa;
         private System.Windows.Forms.DataGridViewImageColumn colCapa;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colTitulo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colAutor;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
-        private System.Windows.Forms.DataGridViewButtonColumn colExcluir;
 
         protected override void Dispose(bool disposing)
         {
@@ -27,14 +22,13 @@
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.cboCategoria = new System.Windows.Forms.ComboBox();
             this.lblStatus = new System.Windows.Forms.Label();
             this.dgvLivros = new System.Windows.Forms.DataGridView();
-            this.picCapa = new System.Windows.Forms.PictureBox();
             this.colCapa = new System.Windows.Forms.DataGridViewImageColumn();
+            this.picCapa = new System.Windows.Forms.PictureBox();
             this.colTitulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -48,7 +42,7 @@
             // 
             this.txtSearch.Location = new System.Drawing.Point(16, 16);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(360, 23);
+            this.txtSearch.Size = new System.Drawing.Size(360, 20);
             this.txtSearch.TabIndex = 0;
             // 
             // btnSearch
@@ -64,9 +58,8 @@
             // 
             this.cboCategoria.Location = new System.Drawing.Point(520, 16);
             this.cboCategoria.Name = "cboCategoria";
-            this.cboCategoria.Size = new System.Drawing.Size(200, 23);
+            this.cboCategoria.Size = new System.Drawing.Size(200, 21);
             this.cboCategoria.TabIndex = 2;
-            this.cboCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
             // 
             // lblStatus
             // 
@@ -74,7 +67,33 @@
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(704, 20);
             this.lblStatus.TabIndex = 3;
-            this.lblStatus.Text = "";
+            // 
+            // dgvLivros
+            // 
+            this.dgvLivros.AllowUserToAddRows = false;
+            this.dgvLivros.AllowUserToDeleteRows = false;
+            this.dgvLivros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLivros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colTitulo,
+            this.colAutor,
+            this.colAno,
+            this.colCategoria,
+            this.colExcluir});
+            this.dgvLivros.Location = new System.Drawing.Point(152, 71);
+            this.dgvLivros.MultiSelect = false;
+            this.dgvLivros.Name = "dgvLivros";
+            this.dgvLivros.RowTemplate.Height = 64;
+            this.dgvLivros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLivros.Size = new System.Drawing.Size(568, 360);
+            this.dgvLivros.TabIndex = 5;
+            // 
+            // colCapa
+            // 
+            this.colCapa.FillWeight = 20F;
+            this.colCapa.HeaderText = "Capa";
+            this.colCapa.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.colCapa.Name = "colCapa";
             // 
             // picCapa
             // 
@@ -85,64 +104,41 @@
             this.picCapa.TabIndex = 4;
             this.picCapa.TabStop = false;
             // 
-            // dgvLivros
-            // 
-            this.dgvLivros.AllowUserToAddRows = false;
-            this.dgvLivros.AllowUserToDeleteRows = false;
-            this.dgvLivros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLivros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colCapa,
-            this.colTitulo,
-            this.colAutor,
-            this.colAno,
-            this.colCategoria,
-            this.colExcluir});
-            this.dgvLivros.Location = new System.Drawing.Point(152, 72);
-            this.dgvLivros.MultiSelect = false;
-            this.dgvLivros.Name = "dgvLivros";
-            this.dgvLivros.RowTemplate.Height = 64;
-            this.dgvLivros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvLivros.Size = new System.Drawing.Size(568, 360);
-            this.dgvLivros.TabIndex = 5;
-            // 
-            // colCapa
-            // 
-            this.colCapa.HeaderText = "Capa";
-            this.colCapa.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
-            this.colCapa.Name = "colCapa";
-            this.colCapa.FillWeight = 20F;
-            // 
             // colTitulo
             // 
+            this.colTitulo.FillWeight = 83.28172F;
             this.colTitulo.HeaderText = "Título";
             this.colTitulo.Name = "colTitulo";
             // 
             // colAutor
             // 
+            this.colAutor.FillWeight = 83.28172F;
             this.colAutor.HeaderText = "Autor";
             this.colAutor.Name = "colAutor";
             // 
             // colAno
             // 
+            this.colAno.FillWeight = 83.28172F;
             this.colAno.HeaderText = "Ano";
             this.colAno.Name = "colAno";
             // 
             // colCategoria
             // 
+            this.colCategoria.FillWeight = 83.28172F;
             this.colCategoria.HeaderText = "Categoria";
             this.colCategoria.Name = "colCategoria";
             // 
             // colExcluir
             // 
+            this.colExcluir.FillWeight = 84.87309F;
             this.colExcluir.HeaderText = "Ações";
             this.colExcluir.Name = "colExcluir";
             this.colExcluir.Text = "Excluir";
             this.colExcluir.UseColumnTextForButtonValue = true;
-            this.colExcluir.FillWeight = 18F;
             // 
             // Form1
             // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 450);
             this.Controls.Add(this.dgvLivros);
@@ -153,10 +149,18 @@
             this.Controls.Add(this.txtSearch);
             this.Name = "Form1";
             this.Text = "OpenLibrary - Busca de Livros";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvLivros)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCapa)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTitulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewButtonColumn colExcluir;
     }
 }
